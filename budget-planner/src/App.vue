89e30f8baad1b9e9c838928c1edb910b02f6d15d
Kweku Martin <kweku.martin@e-farmcredit.com>
2019-11-05 16:02:03 -0500

    Adding implementation of Auth0 authentication; adding new nav bar and profile page

--- budget-planner/src/App.vue
+++ budget-planner/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About Page</router-link>
-    </div>
+    <nav-bar />
     <router-view />
   </div>
 </template>
@@ -46,3 +43,13 @@
   padding: 3px 10px;
 }
 </style>
+
+<script>
+import NavBar from "./components/NavBar";
+
+export default {
+  components: {
+    NavBar
+  }
+};
+</script>
